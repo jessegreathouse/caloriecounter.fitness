495e4994300d2d2b3b594e1c01680b0716c75ed0
Jesse Greathouse <jesse.greathouse@gmail.com>
2014-10-08 15:11:48 -0500

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name caloriecounterfitnessApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the caloriecounterfitnessApp
+ */
+angular.module('caloriecounterfitnessApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
